906ba05329484b64ed0b8267647f095d407ff07d
Tomás Bargut <tomasbarguttabbaj@gmail.com>
2020-03-15 10:10:39 -0300

    Improved setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='horizon',
+    version='1.0',
+    description='Horizon Query Builder',
+    author='Tomás Bargut',
+    author_email='tomasbarguttabbaj@gmail.com',
+    url='https://github.com/tomasbargut/horizon/',
+    packages=find_packages(),
+)
